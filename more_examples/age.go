5000f3ff1fce74a033207769dbc59f6f6e471438
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-11-04 00:50:30 +0530

    added some more examples 1

---
+++ more_examples/age.go
@@ -0,0 +1,22 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	age := 24
+// 	if age < 13 {
+// 		fmt.Println("You are a Young!")
+// 	} else if age < 20 {
+// 		fmt.Println("You are a Teenager!")
+// 	} else if age < 30 {
+// 		fmt.Println("You are in your Twenties!")
+// 	} else if age < 40 {
+// 		fmt.Println("You are in Thirties!")
+// 	} else if age < 50 {
+// 		fmt.Println("You're getting there!")
+// 	} else {
+// 		fmt.Println("Over the hill!")
+// 	}
+// }
---
+++ more_examples/twelveDays.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for day := 1; day <= 12; day++ {
+		fmt.Println("On the", day, "of the Christmas my true love sent to me:")
+		switch day {
+		case 12:
+			fmt.Println("Twelve Drummers Drumming")
+		case 11:
+			fmt.Println("Eleven Pipers Piping")
+		case 10:
+			fmt.Println("Ten Lords a Leaping")
+		case 9:
+			fmt.Println("Nine Ladies Dancing")
+		case 8:
+			fmt.Println("Eight Maids a Milking")
+		case 7:
+			fmt.Println("Seven Swan a Swimming")
+		case 6:
+			fmt.Println("Six Geese a Laying")
+		case 5:
+			fmt.Println("Five Golden Rings")
+		case 4:
+			fmt.Println("Four Calling Birds")
+		case 3:
+			fmt.Println("Three French Hens")
+		case 2:
+			fmt.Println("Two Turtle Doves")
+		case 1:
+			fmt.Println("A Partridge in a Peer tree")
+		}
+	}
+}
